bfe54b938c9281ca4c17011f6a1c1c9e5158a10b
L Frank <Luke.Frank@gmx.net>
2015-12-15 17:12:03 +0100

    Manager

---
+++ app/src/main/java/eva/eva/Manager.java
@@ -0,0 +1,33 @@
+package eva.eva;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TabHost;
+
+/**
+ * Created by Luke on 15.12.2015.
+ */
+public class Manager extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("gruppe");
+        tabSpec.setContent(R.id.tabGruppe);
+        tabSpec.setIndicator("Gruppe");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("bewertung");
+        tabSpec.setContent(R.id.tabBewertung);
+        tabSpec.setIndicator("bewertung");
+        tabHost.addTab(tabSpec);
+
+    }
+}
