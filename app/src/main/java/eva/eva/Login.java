09ad95c5937667d0ae18c12bc5eb1e37d85c89c6
L Frank <Luke.Frank@gmx.net>
2015-12-15 14:48:30 +0100

    Login

--- app/src/main/java/eva/eva/Login.java
+++ app/src/main/java/eva/eva/Login.java
@@ -3,27 +3,41 @@ package eva.eva;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class Login extends ActionBarActivity implements  View.OnClickListener {
 
     Button bLogin;
     EditText etUsername, etPassword;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         etUsername = (EditText) findViewById(R.id.etUsername);
         etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin =(Button) findViewById(R.id.bLogin);
+
+        bLogin.setOnClickListener(this);
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+          case R.id.bLogin:
+
+
+
+                break;
+      }
+    }
 }
